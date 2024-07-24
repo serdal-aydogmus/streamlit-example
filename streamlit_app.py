dce531989712b052f59e1681eea742b8ed99c803
SERDAL AYDOGMUS <saydogmus06@gmail.com>
2024-07-24 10:54:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import streamlit as st
 
 """
 # Welcome to Streamlit!
-
+# Welcome to Streamlit!
+# Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
